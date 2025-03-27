47f8b610cf8f50a112d8d61c34b83d271703e5d6
Vasco Costa <vascomacosta@gmail.com>
2025-03-27 12:34:04 +0000

    Rename tls to tls_dep

--- build.zig
+++ build.zig
@@ -3,16 +3,17 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     const zircon_mod = b.addModule("zircon", .{
         .root_source_file = b.path("src/zircon.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const tls = b.dependency("tls", .{
+    const tls_dep = b.dependency("tls", .{
         .target = target,
         .optimize = optimize,
     });
 
-    zircon_mod.addImport("tls", tls.module("tls"));
+    zircon_mod.addImport("tls", tls_dep.module("tls"));
 }
